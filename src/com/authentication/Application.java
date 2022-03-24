4ffd1c08e1d7c235b1fdd6875af4cc09d2467c0d
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-03-24 15:53:44 +0700

    Create Application.java

---
+++ src/com/authentication/Application.java
@@ -0,0 +1,8 @@
+package com.authentication;
+
+public class Application {
+    public static void main(String[] args) {
+        Controller controller=new Controller();
+        controller.home();
+    }
+}
